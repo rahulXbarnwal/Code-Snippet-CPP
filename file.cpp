cb0971ca3811921dba1dfe189a33520853fb86b2
Rahul Barnwal <78689930+rahulXbarnwal@users.noreply.github.com>
2022-04-20 07:59:47 +0530

    Add files via upload

---
+++ file.cpp
@@ -0,0 +1,66 @@
+/* ucant_know_me*/
+#pragma GCC optimize("O3,unroll-loops")
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define ln "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+#ifdef ucant_know_me
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+typedef map<int,int> m32;
+typedef map<ll,ll> m64;
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+    
+
+    cout<<ln;
+}
+
+int main() {
+    fastio();
+    int t;
+    cin >> t;
+    for(int i = 1; i <= t; i++){
+        //google(i);
+        solve();
+    }
+}
